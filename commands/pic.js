c1f6a717093248ae7c566b84788fde87e3e99270
emilieea88 <61885885+emilieea88@users.noreply.github.com>
2021-11-24 02:48:27 +0000

    improved pic command again

--- commands/pic.js
+++ commands/pic.js
@@ -1,17 +1,13 @@
 const fs = require('fs');
 const photos = './photos';
 
-const bomel = fs.readdirSync(photos).length
-console.log(bomel)
-const buml = Math.floor(Math.random()*bomel);
-
-const str1 = 'https://raw.githubusercontent.com/emilieea88/BumbyBot/main/photos/bumble_'
-const str3 = '.jpg'
+const str1 = 'https://raw.githubusercontent.com/emilieea88/BumbyBot/main/photos/bumble_';
+const str3 = '.jpg';
 
 module.exports = {
     name: 'pic',
     description: 'random bumby pic',
     execute(message, args){
-        message.channel.send(str1.concat(String(buml), str3));
+        message.channel.send(str1.concat(String((Math.floor(Math.random()*(fs.readdirSync(photos).length)))), str3));
     }
 }
